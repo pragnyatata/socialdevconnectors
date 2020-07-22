bf1d0b76929e1413faafe30a11097bacc9223eac
tata2501 <pragnya25012000@gmail.com>
2020-07-23 02:14:41 +0530

    deployment files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 const userRoutes = require("./routes/api/users");
 const authRoutes = require("./routes/api/auth");
@@ -10,14 +11,17 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  return res.json({ messgae: "Test working" });
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 
